2244e3bd46eeca73369f89bc77421dcb6e2f2811
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-05-27 01:47:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -13,7 +13,7 @@ async def subscribe(app, message):
         try:
           user = await app.get_chat_member(FORCE_SUB, message.from_user.id)
           if str(user.status) == "ChatMemberStatus.BANNED":
-              await message.reply_text("You are Banned. Contact -- @UncleChipssBot")
+              await message.reply_text("You are Banned. Contact -- @UncleChipssBot to use the bot again")
               return 1
         except UserNotParticipant:
             link = await app.export_chat_invite_link(FORCE_SUB)
@@ -199,7 +199,7 @@ async def plan(client, message):
 @app.on_callback_query(filters.regex("see_plan"))
 async def see_plan(client, callback_query):
     plan_text = (
-        "> 💰**Premium Price**\n\n Starting from $2 or 200 INR accepted via **__Amazon Gift Card__** (terms and conditions apply).\n"
+        "> 💰**Lifetime Premium Price**\n\n $2 or 200 INR accepted via **__Google Play Redeem Code__** (terms and conditions apply).\n"
         "📥 **Download Limit**: Users can download up to 100,000 files in a single batch command.\n"
         "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
         "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
